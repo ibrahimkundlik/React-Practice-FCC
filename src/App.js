a86d9161183c82ef10135ab65ec51908c86fa518
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-03-23 23:30:30 +0530

    Added Form Component

--- src/App.js
+++ src/App.js
@@ -2,39 +2,49 @@ import React, { useState } from "react";
 import BookList from "./components/booklist/BookList.component";
 import UseEffect from "./components/useEffect/UseEffect.component";
 import UseState from "./components/useState/UseState.component";
+import Forms from "./components/forms/forms.component";
 import "./index.styles.scss";
 
 function App() {
-	const [toggle, setToggle] = useState([false, false, false]);
+	const [toggle, setToggle] = useState([false, false, false, false]);
 
 	return (
 		<>
 			<div className="toggle-cont">
 				<button
 					onClick={() => {
-						setToggle([true, false, false]);
+						setToggle([true, false, false, false]);
 					}}
 				>
 					Book Store
 				</button>
 				<button
 					onClick={() => {
-						setToggle([false, true, false]);
+						setToggle([false, true, false, false]);
 					}}
 				>
 					UseState
 				</button>
 				<button
 					onClick={() => {
-						setToggle([false, false, true]);
+						setToggle([false, false, true, false]);
 					}}
 				>
 					UseEffect
+				</button>
+
+				<button
+					onClick={() => {
+						setToggle([false, false, false, true]);
+					}}
+				>
+					Forms
 				</button>
 			</div>
 			{toggle[0] && <BookList />}
 			{toggle[1] && <UseState />}
 			{toggle[2] && <UseEffect />}
+			{toggle[3] && <Forms />}
 		</>
 	);
 }
---
+++ src/components/forms/forms.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./forms.styles.scss";
+
+const Forms = () => {
+	return <div>Forms</div>;
+};
+
+export default Forms;
