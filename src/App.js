91d15375dbe78f0a2eeee454083a01e0995e7e38
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-03-24 23:07:27 +0530

    Displaying multiple pages in App.js

--- src/App.js
+++ src/App.js
@@ -6,45 +6,12 @@ import Forms from "./components/forms/forms.component";
 import "./index.styles.scss";
 
 function App() {
-	const [toggle, setToggle] = useState([false, false, false, false]);
-
 	return (
 		<>
-			<div className="toggle-cont">
-				<button
-					onClick={() => {
-						setToggle([true, false, false, false]);
-					}}
-				>
-					Book Store
-				</button>
-				<button
-					onClick={() => {
-						setToggle([false, true, false, false]);
-					}}
-				>
-					UseState
-				</button>
-				<button
-					onClick={() => {
-						setToggle([false, false, true, false]);
-					}}
-				>
-					UseEffect
-				</button>
-
-				<button
-					onClick={() => {
-						setToggle([false, false, false, true]);
-					}}
-				>
-					Forms
-				</button>
-			</div>
-			{toggle[0] && <BookList />}
-			{toggle[1] && <UseState />}
-			{toggle[2] && <UseEffect />}
-			{toggle[3] && <Forms />}
+			{/* <BookList /> */}
+			{/* <UseState /> */}
+			{/* <UseEffect /> */}
+			<Forms />
 		</>
 	);
 }
